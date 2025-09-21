600386653b0daf2755a555cc3be622364c1ae128
ZaRaza995 <pvladik152007@gmail.com>
2025-09-21 20:37:41 +0300

    lab02 ex 1

---
+++ src/lab02/ex01.py
@@ -0,0 +1,48 @@
+def min_max(nums):
+    if len(nums) == 0:
+        raise ValueError("Список пуст")
+    return min(nums), max(nums)
+
+
+def unique_sorted(nums):
+    return sorted(set(nums))
+
+
+def flatten(matrix):
+    result = []
+    for row in matrix:
+        if not isinstance(row, (list, tuple)):
+            raise TypeError("Внутри могут быть только списки или кортежи")
+        
+        for item in row:
+            result.append(item)
+            
+    return result
+
+print("min_max ")
+print(min_max([3, -1, 5, 0]))
+print(min_max([42]))
+print(min_max([-5, -2, -9]))
+
+# Пробуем вызвать функцию с пустым списком
+try:
+    min_max([])
+except ValueError:
+    print("[] → ValueError")
+
+
+print("\n unique_sorted ")
+print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([]))
+print(unique_sorted([-1, 0, -1, 2]))
+
+
+print("\n flatten ")
+print(flatten([[1, 2], [3, 4]]))
+print(flatten( ((1, 2), (3, 4, 5)) ))
+
+# Точно так же ловим ошибку TypeError для функции flatten
+try:
+    flatten([[1], "a"])
+except TypeError:
+    print("[[1], 'a'] → TypeError")
